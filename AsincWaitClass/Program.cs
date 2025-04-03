aa98ba354ed8eaed85fe367d5c7b76c80dd5c46f
LukashykYevhenii <lukashyk.yevhenii@chnu.edu.ua>
2025-04-03 20:57:39 +0300

    Lesson11-12 Async-p1

---
+++ AsincWaitClass/Program.cs
@@ -0,0 +1,29 @@
+﻿using System.Text;
+
+class MyClass
+{
+    public static void Main(string[] args)
+    {
+        FileStream fileStream = new FileStream(
+            "C:\\Users\\Home\\RiderProjects\\SystemPrpgramingP32_1\\AsincWaitClass\\text",
+            FileMode.Open, FileAccess.Read, FileShare.Read, 1024, FileOptions.Asynchronous);
+
+        Byte[] buffer = new Byte[100];
+        IAsyncResult result = fileStream.BeginRead(buffer, 0, buffer.Length, null, null);
+
+
+        while (result.IsCompleted == false)
+        {
+            Console.WriteLine("Waiting for file...");
+            Thread.Sleep(100);
+        }
+
+        Int32 readBytes = fileStream.EndRead(result);
+
+        fileStream.Close();
+
+        Console.WriteLine("Read {0} bytes", readBytes);
+        Console.WriteLine(Encoding.UTF8.GetString(buffer));
+
+    }
+}
---
+++ PoolApp/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Threading;
+
+class MyClass
+{
+    public static void Main(string[] args)
+    {
+        Console.WriteLine("Main thread started: queue a working element");
+        Random rnd = new Random();
+        for (int i = 0; i < 10; i++)
+        {
+            ThreadPool.QueueUserWorkItem(WorkingElementMethod, rnd.Next( 10));
+        }
+        Console.WriteLine("Main thread finished: execute other tasks");
+        Thread.Sleep(1000);
+        Console.WriteLine("Press any key to exit...");
+        Console.ReadKey();
+    }
+
+
+    private static void WorkingElementMethod(object state)
+    {
+        Console.WriteLine("\tThread: " + Thread.CurrentThread.ManagedThreadId + " state: " + state);
+        Thread.Sleep(1000);
+    } 
+}
