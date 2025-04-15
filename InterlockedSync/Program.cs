fdcddf7bed1715fa86bfeda6371361de5fa6fe23
LukashykYevhenii <lukashyk.yevhenii@chnu.edu.ua>
2025-04-15 20:34:05 +0300

    Lesson15-16-17-18 solve

---
+++ InterlockedSync/Program.cs
@@ -0,0 +1,50 @@
+﻿class InterlockedCounter
+{
+    int field1;
+    int field2;
+ 
+    public int Field1
+    {
+        get { return field1; }
+    }
+ 
+    public int Field2
+    {
+        get { return field2; }
+    }
+ 
+    public void UpdateFields()
+    {
+       Console.WriteLine("Start:" + DateTime.Now.ToLongTimeString());
+
+        
+        for (int i = 0; i < 100_000_000; ++i)
+        {
+           Interlocked.Increment(ref field1);
+           if (field1 % 2 == 0)
+               Interlocked.Increment(ref field2);
+        }
+        Console.WriteLine("End:" + DateTime.Now.ToLongTimeString());
+    }
+}
+
+class MyClass
+{
+    public static void Main(string[] args)
+    {
+            Console.WriteLine("Синхронізація Interlocked-методами:");
+            InterlockedCounter c = new InterlockedCounter();
+ 
+            Thread[] threads = new Thread[5];
+            for (int i = 0; i < threads.Length; ++i)
+            {
+                threads[i] = new Thread(c.UpdateFields);
+                threads[i].Start();
+            }
+ 
+            for (int i = 0; i < threads.Length; ++i)
+                threads[i].Join();
+
+            Console.WriteLine("Field1: {0}, Field2: {1}\n\n", c.Field1, c.Field2);
+    }
+}
---
+++ MutexExample/Program.cs
@@ -0,0 +1,43 @@
+﻿class MyClass
+{
+    public static void Main(string[] args)
+    {
+        Console.WriteLine("Mutex Synchronization:");
+        Counter counter = new Counter();
+        Thread[] threads = new Thread[5];
+        
+        for (int i = 0; i < threads.Length; i++)
+        {
+            threads[i] = new Thread(counter.UpdateCount);
+            threads[i].Start();
+        }
+        
+
+    }
+}
+
+class Counter
+{
+    int count;
+    Mutex mutex = new Mutex(false, "SYNC_MUTEX");
+
+    public int Count
+    {
+        get { return count; }
+    }
+
+    public void UpdateCount()
+    {
+        Console.WriteLine("Start:" + DateTime.Now.ToLongTimeString());
+
+        for (int i = 0; i < 1_000_000; i++)
+        {
+            mutex.WaitOne();
+            ++count;
+            mutex.ReleaseMutex();
+        }
+        
+        Console.WriteLine("End:" + DateTime.Now.ToLongTimeString());
+
+    }
+}
